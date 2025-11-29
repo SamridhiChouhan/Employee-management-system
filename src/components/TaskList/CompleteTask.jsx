c861cbb8278d9ee59e10acffd511409a06a960dd
SamridhiChouhan <samridhichouhan1153@gmail.com>
2025-11-29 22:31:05 +0530

    Task components dynamic

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const CompleteTask = () => {
+const CompleteTask = ({ data }) => {
   return (
-    <div className="flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl p-5">
-      <div className="flex justify-between items-center">
-        <h3 className="bg-red-600 px-3 py-1 rounded text-sm">High</h3>
-        <h4 className="text-sm">20 feb 2024</h4>
+    <div className="flex-shrink-0 flex flex-col justify-between h-full w-[300px] bg-red-400 rounded-xl p-5">
+      <div>
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-3 py-1 rounded text-sm">
+            {data.category}
+          </h3>
+          <h4 className="text-sm">{data.date}</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">{data.title}</h2>
+        <p className="text-sm mt-2">{data.description}</p>
       </div>
-      <h2 className="mt-5 text-2xl font-semibold">Make a project</h2>
-      <p className="text-sm mt-2">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa veniam
-        optio quisquam, soluta excepturi reprehenderit?
-      </p>
-
       <div className="w-full mt-5">
         <button className="w-full text-sm bg-green-500 text-white py-2 rounded-md">
           Completed
